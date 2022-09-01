b6abffa10467fbe254558ad6559b9f5a4a11cdad
Larry Zhang <larryz@my.yorku.ca>
2022-08-31 22:52:49 -0400

    Configured CORS

--- app.js
+++ app.js
@@ -28,7 +28,21 @@ const appleTreeSchema = {
 const AppleTree = mongoose.model("AppleTree", appleTreeSchema);
 
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+});
 
 app.route("/appleTrees")
 
